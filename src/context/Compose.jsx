818c4f1f41a9b4b8493f86fbea93a7635c5e1cc2
Mohammad Azimi <mohammadazimi91@gmail.com>
2023-02-03 23:40:38 +0330

    added Compose for context

---
+++ src/context/Compose.jsx
@@ -0,0 +1,14 @@
+function Compose({ components, children }) {
+  return (
+    <>
+      {components.reverse().reduce((acc, curr) => {
+        const [Provider, props] = Array.isArray(curr)
+          ? [curr[0], curr[1]]
+          : [curr, {}];
+        return <Provider {...props}>{acc}</Provider>;
+      }, children)}
+    </>
+  );
+}
+
+export default Compose;
