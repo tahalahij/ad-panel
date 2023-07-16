8067e69f24c0d69eceacd9adf82475a43c65c8f6
Mohammad Azimi <mohammadazimi91@gmail.com>
2023-07-16 11:44:34 +0330

    fix: fetch operators list only if admin or controller

--- src/layouts/list/useOperatorData.tsx
+++ src/layouts/list/useOperatorData.tsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react";
-import { getControllerListRequest, getOperatorListRequest } from "../../network/requests";
+import {
+  getControllerListRequest,
+  getOperatorListRequest,
+} from "../../network/requests";
 import { USER_ROLE, User } from "../../types/UserTypes";
+import { useAuthenticationState } from "../../context";
+import { userHasAccess } from "../../utils/UserAccess";
 
 export const useOperatorData = (userType: USER_ROLE) => {
   const [userList, setUserList] = useState<User[]>();
   const [loading, setLoading] = useState(false);
+  const authState = useAuthenticationState();
 
   const fetchData = async (page: number = 0) => {
+    if (!userHasAccess(authState.role, ["ADMIN", "CONTROLLER"])) return;
+
     setLoading(true);
     const apiRequest =
       userType.toLocaleLowerCase() === "operator"
